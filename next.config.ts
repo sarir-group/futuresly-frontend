a029639effbbec2c8de2de772b8746082d6d3a26
amirhossein693 <amirhossein693@gmail.com>
2025-01-04 13:28:18 +0330

    rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: '/:path*{/}?',
+        has: [
+          {
+            type: 'host',
+            value: '(?<siteHost>.*)',
+          },
+        ],
+        destination: '/site/:siteHost/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/site/[siteHost]/[path]/page.tsx
+++ src/app/site/[siteHost]/[path]/page.tsx
@@ -1,12 +1,20 @@
-
-export default function Home() {
+export default async function Page({
+  params,
+}: {
+  params: {
+    siteHost: string;
+    path: string;
+  };
+}) {
+  const { path, siteHost } = await params
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       futuresly
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-
+        <div>{siteHost}</div>
+        <div>{path}</div>
       </footer>
     </div>
   );
