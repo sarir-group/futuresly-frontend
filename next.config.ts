875c560aee5dc2996ae13bb0500bd78fa96e576f
amirhossein693 <amirhossein693@gmail.com>
2025-01-04 14:04:17 +0330

    dynamic params

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: '/:path*{/}?',
-        has: [
-          {
-            type: 'host',
-            value: '(?<siteHost>.*)',
-          },
-        ],
-        destination: '/site/:siteHost/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
--- src/app/[lang]/[[...slug]]/page.tsx
+++ src/app/[lang]/[[...slug]]/page.tsx
@@ -1,18 +1,18 @@
 export default async function Page({
   params,
 }: {
-  params: Promise<{ path: string; siteHost: string }>;
+  params: Promise<{ lang: string; slug: string }>
 }) {
-  const { path, siteHost } = await params
+  const { lang, slug } = await params;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      futuresly
+        futuresly
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <div>{siteHost}</div>
-        <div>{path}</div>
+        <div>{lang.toString()}</div>
+        <div>{slug.toString()}</div>
       </footer>
     </div>
-  );
+  )
 }
