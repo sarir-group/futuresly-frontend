23a4ceb6baa1855471ead77ef97c628154696498
amirhossein693 <amirhossein693@gmail.com>
2025-01-07 14:19:32 +0330

    page slug

--- src/app/page/[[...slug]]/page.tsx
+++ src/app/page/[[...slug]]/page.tsx
@@ -16,7 +16,7 @@ export default async function Page({
         futuresly
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center text-xs">
-        <div>Page{slug && <>/ {(slug || [])?.join('/')}</>}</div>
+        <div>Page{slug && <>/{(slug || [])?.join('/')}</>}</div>
         <div>{slug && <>slug: {slug?.toString()}</>}</div>
         <div>locale: {locale}</div>
         <div>{t('title')}</div>
